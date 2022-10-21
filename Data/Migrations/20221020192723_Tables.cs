acaeda6ec9d53515021ce252e1aba9c232808f7d
wilk junior <wilkjunior69@gmail.com>
2022-10-21 19:43:04 -0300

    Adição da pasta Helpers.

---
+++ Data/Migrations/20221020192723_Tables.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace salaodebeleza.Data.Migrations
+{
+    public partial class Tables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Agendamentos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ClienteID = table.Column<int>(type: "integer", nullable: false),
+                    DataAgendamento = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agendamentos", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clientes",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    CPF = table.Column<string>(type: "character varying(14)", maxLength: 14, nullable: false),
+                    DataNascimento = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clientes", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Servicos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Preco = table.Column<double>(type: "double precision", nullable: false),
+                    Descricao = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Servicos", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Agendamentos");
+
+            migrationBuilder.DropTable(
+                name: "Clientes");
+
+            migrationBuilder.DropTable(
+                name: "Servicos");
+        }
+    }
+}
---
+++ Helpers/JS.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace salaodebeleza.Helpers{
+    public static class JS{
+        public static ValueTask AlertMessage (this IJSRuntime js, string message)
+        {
+            return js.InvokeVoidAsync ("alertMessage", message);
+        }
+        
+        public static ValueTask SweetAlert(this IJSRuntime js, string message)
+        {
+            return js.InvokeVoidAsync("SweetAlert", message);
+        }
+
+
+    }
+}
