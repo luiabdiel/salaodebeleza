f5404e0692b18cd94b4f7e8881df8c04bf37f246
JuanLeitte <juanleitereis@gmail.com>
2022-10-28 16:08:03 -0300

    Correção de Bugs

---
+++ Helpers/Formating.cs
@@ -0,0 +1,13 @@
+﻿namespace salaodebeleza.Helpers
+{
+    
+    public static class Formating
+    {
+        public static string Formatado(this double valor) =>
+            $"R${valor.ToString(".00")}";
+
+        public static string FormatadoCount(this int count) =>
+            $"{count.ToString("00")}";
+    }
+    
+}
