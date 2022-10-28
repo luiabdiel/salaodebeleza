55d08e44e7230ac5e3fea1a27d7caa4571650d6f
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2022-10-28 15:27:31 -0300

    fix: erro de sintaxe;

--- Models/Venda.cs
+++ Models/Venda.cs
@@ -5,27 +5,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace salaodebeleza.Models;
 public class Venda
 {
-
-        public Venda()
-        {
+    public Venda()
+    {
         DataEmissao = DateTime.Now;
         Itens = new();
-        }
+    }
 
-        [Key]
-        public int ID { get; set; }
+    [Key]
+    public int ID { get; set; }
 
     public DateTime DataEmissao { get; set; }
 
-        [Range(1, int.MaxValue)]
-        [ForeignKey("ClienteID")]
-        public int ClienteID { get; set; }
+    [Range(1, int.MaxValue)]
+    [ForeignKey("ClienteID")]
+    public int ClienteID { get; set; }
 
-        public Cliente Cliente { get; set; }
+    public Cliente Cliente { get; set; }
 
     [QuantidadeItens]
-        public List<VendaItem> Itens { get; set; }
-
-
-    }
+    public List<VendaItem> Itens { get; set; }
 }
